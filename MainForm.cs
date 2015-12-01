36d6d62f4ea4591d33f9b29de1df06aa8b91745b
Dirk.Buck <dirk.buck@alpha-com.de>
2015-12-01 12:24:53 +0100

    Initialer Commit SharpDev Projekt erstellt

---
+++ MainForm.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Erstellt mit SharpDevelop.
+ * Benutzer: buck
+ * Datum: 12/01/2015
+ * Zeit: 12:16
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace getScreenShot
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Erstellt mit SharpDevelop.
+ * Benutzer: buck
+ * Datum: 12/01/2015
+ * Zeit: 12:16
+ * 
+ */
+using System;
+using System.Windows.Forms;
+
+namespace getScreenShot
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
