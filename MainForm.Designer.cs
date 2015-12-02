36b96fbcf546f9b11282609835961b15e2ae5004
Dirk.Buck <dirk.buck@alpha-com.de>
2015-12-02 14:25:03 +0100

    Upd: Minimalistische GUI. Mit Button zum Screenshot erstellen.

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -13,6 +13,7 @@ namespace getScreenShot
 		/// Designer variable used to keep track of non-visual components.
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -35,12 +36,35 @@ namespace getScreenShot
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(22, 16);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
 			// MainForm
 			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "getScreenShot";
+			this.ClientSize = new System.Drawing.Size(138, 62);
+			this.Controls.Add(this.button1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
 			this.Name = "MainForm";
+			this.Text = "getScreenShot";
+			this.Shown += new System.EventHandler(this.MainForm_Shown);
+			this.ResumeLayout(false);
+
 		}
 	}
 }
