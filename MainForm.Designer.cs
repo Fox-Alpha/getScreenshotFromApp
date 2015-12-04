fc16a56857c28969d4c1eb35ced1fc9d4787a1d7
Dirk.Buck <dirk.buck@alpha-com.de>
2015-12-04 16:03:54 +0100

    Add: Checkbox für LogOnly eingefügt

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -18,6 +18,7 @@ namespace getScreenShot
 		private System.Windows.Forms.StatusStrip statusStrip1;
 		private System.Windows.Forms.TextBox textBox1;
 		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.CheckBox checkBox1;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -47,6 +48,7 @@ namespace getScreenShot
 			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.label1 = new System.Windows.Forms.Label();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
 			this.SuspendLayout();
 			// 
 			// button1
@@ -55,7 +57,7 @@ namespace getScreenShot
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(75, 27);
 			this.button1.TabIndex = 0;
-			this.button1.Text = "button1";
+			this.button1.Text = "Ausführen";
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
@@ -66,33 +68,45 @@ namespace getScreenShot
 			// 
 			// statusStrip1
 			// 
-			this.statusStrip1.Location = new System.Drawing.Point(0, 71);
+			this.statusStrip1.Location = new System.Drawing.Point(0, 97);
 			this.statusStrip1.Name = "statusStrip1";
-			this.statusStrip1.Size = new System.Drawing.Size(108, 22);
+			this.statusStrip1.Size = new System.Drawing.Size(106, 22);
 			this.statusStrip1.TabIndex = 1;
 			this.statusStrip1.Text = "statusStrip1";
 			// 
 			// textBox1
 			// 
-			this.textBox1.Location = new System.Drawing.Point(0, 44);
+			this.textBox1.Location = new System.Drawing.Point(0, 74);
 			this.textBox1.Name = "textBox1";
 			this.textBox1.Size = new System.Drawing.Size(100, 20);
 			this.textBox1.TabIndex = 2;
+			this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(0, 28);
+			this.label1.Location = new System.Drawing.Point(0, 58);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(70, 13);
 			this.label1.TabIndex = 3;
 			this.label1.Text = "Prozessname";
 			// 
+			// checkBox1
+			// 
+			this.checkBox1.Location = new System.Drawing.Point(0, 34);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(104, 24);
+			this.checkBox1.TabIndex = 4;
+			this.checkBox1.Text = "Nur Prozess Log";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(108, 93);
+			this.ClientSize = new System.Drawing.Size(106, 119);
+			this.Controls.Add(this.checkBox1);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.textBox1);
 			this.Controls.Add(this.statusStrip1);
